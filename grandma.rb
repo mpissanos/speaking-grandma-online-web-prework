14472ef7198add60121ca0f32d38ba5884d72e0d
Manny Pissanos <mpissano@gmail.com>
2018-08-30 15:42:54 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(phrase)
   else if phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   else
-    return "HUH?! SPEAK UP SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
    end
  end
 end
